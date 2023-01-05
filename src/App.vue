19e0a44d6fd1b042f59a30688e6d01502249fcaf
Gustavo de Camargo Campos <contato.wanjalagus@oulook.com.br>
2023-01-04 21:23:10 -0400

    (dev) selecionado a paleta de cores do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,9 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+  <router-view>
+  </router-view>
 </template>
 
 <script>
-
 export default {
   name: 'App',
 
@@ -53,3 +12,12 @@ export default {
   }),
 };
 </script>
+<style>
+:root {
+  --primary-color-pale-spring-bud: #CCD5AE ;
+  --primary-color-cornsilk: #E9EDC9 ;
+  --primary-color-papaya-whip: #FEFAE0 ;
+  --primary-color-fawn: #FAEDCD ;
+  --secundary-color-pale-spring-bud: #D4A373 ;
+}
+</style>
---
+++ src/views/Login/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="login">
+    <div id="logo-empresa">
+      <h1>Logo da empresa</h1>
+    </div>
+    <div id="container-login">
+      <v-form>
+        <v-text-field label="Insira seu usuário" v-model="usuario"></v-text-field>
+        <v-text-field label="Insira sua senha" v-model="senha" type="password"></v-text-field>
+      </v-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    usuario: '',
+    senha: '',
+  }),
+};
+</script>
+
+<style>
+#login {
+  background-color: --primary-color-pale-spring-bud;
+}
+</style>
